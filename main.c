f1c12a34b389f257ae2b0d18c2b4f5659fffd4f2
Souvik Basak <sbasak50@stuy.edu>
2024-11-25 15:35:11 -0500

    end

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+void childInstruction() {
+  // int randomFile = open("/dev/urandom", O_RDONLY, 0);
+  // int r;
+  // read(randomFile, &r, 1);
+  // r = r%5;
+  // printf("%d %d\n", getpid(), r);
+  // close(randomFile);
+}
 
 int main() {
-  pid_t p = fork();
-  pid_t p2;
+  pid_t p, p2;
+  p = fork();
   if (p < 0) {
     perror("fork failed");
     exit(1);
   } else if (p == 0) {
-    printf("Child 1\n");
+    childInstruction();
   } else {
     p2 = fork();
   }
   if (p2 < 0) {
     perror("fork failed");
+    exit(1);
   } else if (p == 0) {
-    printf("Child 2\n");
+    childInstruction();
   }
 }
