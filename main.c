0032e39ff20ae5f9eb97fc6b695ba13a839f4c81
Souvik Basak <souvik.b416@gmail.com>
2024-11-26 10:16:31 -0500

    did work

--- main.c
+++ main.c
@@ -2,33 +2,48 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
 
 void childInstruction() {
-  // int randomFile = open("/dev/urandom", O_RDONLY, 0);
-  // int r;
-  // read(randomFile, &r, 1);
-  // r = r%5;
-  // printf("%d %d\n", getpid(), r);
-  // close(randomFile);
+   int randomFile = open("/dev/urandom", O_RDONLY, 0);
+   int r = 0;
+   read(randomFile, &r, 1);
+   r = r % 5 + 1;
+   printf("%d %dsec\n", getpid(), r);
+   close(randomFile);
+   sleep(r);
+   printf("%d finished after %dsec\n", getpid(), r);
 }
 
 int main() {
   pid_t p, p2;
+  int status;
+  printf("%d about to create 2 child processes\n", getpid());
+
   p = fork();
   if (p < 0) {
     perror("fork failed");
     exit(1);
   } else if (p == 0) {
     childInstruction();
-  } else {
-    p2 = fork();
+    exit(0);
   }
+
+  p2 = fork();
   if (p2 < 0) {
     perror("fork failed");
     exit(1);
-  } else if (p == 0) {
+  } else if (p2 == 0) {
     childInstruction();
+    exit(0);
+  }
+
+  pid_t child = wait(&status);
+  if (child > 0) {
+    if (WIFEXITED(status)) {
+        printf("Main Process %d is done. Child %d slept for \n", getpid(), child);
+    }
   }
 }
