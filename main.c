180bc51b7a7b3517d4edde1b7296b2709dcdd054
Souvik Basak <sbasak50@stuy.edu>
2024-11-26 15:01:58 -0500

    child processes return time

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <errno.h>
 #include <string.h>
 
-void childInstruction() {
+int childInstruction() {
    int randomFile = open("/dev/urandom", O_RDONLY, 0);
    int r = 0;
    read(randomFile, &r, 1);
@@ -15,6 +15,7 @@ void childInstruction() {
    close(randomFile);
    sleep(r);
    printf("%d finished after %dsec\n", getpid(), r);
+   return r;
 }
 
 int main() {
@@ -27,8 +28,7 @@ int main() {
     perror("fork failed");
     exit(1);
   } else if (p == 0) {
-    childInstruction();
-    exit(0);
+    return childInstruction();
   }
 
   p2 = fork();
@@ -36,14 +36,13 @@ int main() {
     perror("fork failed");
     exit(1);
   } else if (p2 == 0) {
-    childInstruction();
-    exit(0);
+    return childInstruction();
   }
 
   pid_t child = wait(&status);
   if (child > 0) {
     if (WIFEXITED(status)) {
-        printf("Main Process %d is done. Child %d slept for \n", getpid(), child);
+        printf("Main Process %d is done. Child %d slept for %d seconds\n", getpid(), child, WEXITSTATUS(status));
     }
   }
 }
